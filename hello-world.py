47f8cb02a5fe7e9154cd52677e77f8473d38fcee
lifeng <459559238@qq.com>
2021-07-21 13:10:20 +0800

    init

---
+++ hello-world.py
@@ -0,0 +1,19 @@
+"""
+Airflow的第一个DAG
+"""
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from datetime import datetime
+
+
+default_args = {
+    "owner": "smiecj",
+    "start_date": datetime(2021, 7, 21)
+}
+
+dag = DAG("Hello-World", 
+        description="My first dag",
+        default_args=default_args, 
+        schedule_interval='0 * * * *')
+
+t1 = BashOperator(task_id="hello", bash_command="echo 'Hello World, today is {{ ds }}'", dag=dag)
